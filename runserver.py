25db789d3920332ef31d7943321bbedce82e22a8
annye braca <annyebraca@gmail.com>
2019-06-19 16:11:47 +0100

    Update server file

--- runserver.py
+++ runserver.py
@@ -4,5 +4,8 @@ This script runs the My_site application using a development server.
 
 from Room_App import app
 import os
+
+
+app.secret_key = os.urandom(24)
 port = int(os.environ.get('PORT',8000))
 app.run(debug=True, port=8000)
